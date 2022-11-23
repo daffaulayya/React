fa6e55acf68d1c3f98adc07c5e1abdde775dad41
Daffa_Ulayya_Suhendra <daffau38@gmail.com>
2022-11-23 16:23:43 +0700

    bootstrap-react

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { Hello, HelloClass } from "./intro/Hello";
+import { Col, Container, Row } from "react-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <Hello />
-      <HelloClass />
-    </div>
+    <Container fluid>
+      <Row>
+        <Col>
+          <Hello />
+        </Col>
+        <Col>
+          <HelloClass />
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
